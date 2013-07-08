a3553440bd19a4a17bd3666511192a3463a2685c
Hunter Owens <owens.hunter@gmail.com>
2013-07-08 12:09:30 -0500

    bash/perl script to convert TSV to CSV

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_ind_minneapolis.csv > bike_ind_minneapolis_c.csv
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_agg_minneapolis.csv > bike_agg_minneapolis_c.csv
+echo "done with minneapolis"
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_ind_newyork.csv > bike_ind_newyork_c.csv
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_agg_newyork.csv > bike_agg_newyork_c.csv
+echo "done with NY"
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_ind_boston.csv > bike_ind_boston_c.csv
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_agg_boston.csv > bike_agg_boston_c.csv
+echo "done with Bos"
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_ind_chicago.csv > bike_ind_chicago_c.csv
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_agg_chicago.csv > bike_agg_chicago_c.csv
+echo "done with chicago"
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_ind_washingtondc.csv > bike_ind_washingtondc_c.csv
+perl -lpe 's/"/""/g; s/^|$/"/g; s/\t/","/g' < bike_agg_washingtondc.csv > bike_agg_washingtondc_c.csv
+echo "finished"
