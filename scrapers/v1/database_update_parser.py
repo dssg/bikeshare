e059beeb56f6ea062da82a3336e0a6a43c3a4c99
Hunter Owens <owens.hunter@gmail.com>
2013-07-10 17:41:25 -0500

    broken stuff for vitter

---
+++ scrapers/v1/database_update_parser.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from types import NoneType
+from sys import argv
+import datetime
+import calendar
+from psycopg2 import *
+
+def extract_str(element):
+    if type(element) is NoneType:
+        return element
+    else:
+        return element.string
+
+def main(url):
+    r = requests.get(url)
+
+    fulltext = r.text
+
+    soup = BeautifulSoup(fulltext)
+    print soup.find_all("station")[1]
+    for station in soup.find_all("station"):
+        ident = extract_str(station.find("id"))
+        # bikes = int(station.find("nbBikes"))
+        stations = int(station.find("nbEmptyDocks"))
+        timestamp = datetime.datetime.utcnow()
+        print "{0},{1},{2},{3}".format(ident, bikes, stations, timestamp)
+
+
+# #urls = ("http://www.thehubway.com/data/stations/bikeStations.xml", "https://secure.niceridemn.org/data2/bikeStations.xml",
+#        "http://www.thehubway.com/data/stations/bikeStations.xml")
+
+main(argv[1])
+    
