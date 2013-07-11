30c5cf1cc4f563942e007c37365fd76281401773
Hunter Owens <owens.hunter@gmail.com>
2013-07-11 14:32:30 -0500

    adding the script for the crontab #12 #9

--- scrapers/v1/database_update_parser.py
+++ scrapers/v1/database_update_parser.py
@@ -12,7 +12,7 @@ def extract_str(element):
         return element
     else:
         return element.string
-
+##This function connects to the data base (through enviornment varibles), gets the data from a provided url
 def main(url,city):
     try:
         conn = psycopg2.connect("dbname="+os.environ.get('dbname')+" user="+os.environ.get('dbuser')+ " host="+os.environ.get('dburl'))
@@ -31,21 +31,20 @@ def main(url,city):
         bikes = long(extract_str(station.find("nbbikes")))
         stations = long(extract_str(station.find("nbemptydocks")))
         timestamp = datetime.datetime.utcnow()
-        #print ident+','+bikes+','+stations
         if (city.lower() == 'boston'):
             cur.execute("""INSERT INTO bike_ind_boston (tfl_id, bikes, spaces) VALUES (%s, %s,%s);""",(ident,bikes,stations))
-        elif (city == "washingtondc"):
+        elif (city.lower() == "washingtondc"):
             cur.execute("""INSERT INTO bike_ind_washingtondc (tfl_id, bikes, spaces, timestamp) VALUES (%s, %s,%s,%s);""",(ident, bikes,stations,timestamp))
-        elif (city == "minneapolis"):
+        elif (city.lower() == "minneapolis"):
             cur.execute("""INSERT INTO bike_ind_minneapolis (tfl_id, bikes, spaces, timestamp) VALUES (%s, %s,%s,%s);""",(ident, bikes,stations,timestamp))
         else:
             print "no city info supplied"
-        #print timestamp
     conn.commit()
     #print timestamp
 
 # #urls = ("http://www.thehubway.com/data/stations/bikeStations.xml", "https://secure.niceridemn.org/data2/bikeStations.xml",
-#        "http://www.thehubway.com/data/stations/bikeStations.xml")
+#        "http://www.capitalbikeshare.com/data/stations/bikeStations.xml")
 
+##parameters URL and City [boston,washingtondc,minneapolis]
 main(argv[1],argv[2])
     
