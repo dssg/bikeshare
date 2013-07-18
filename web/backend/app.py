4670aece23da98455ed61c5e0a12bb492aed56e2
Hunter Owens <owens.hunter@gmail.com>
2013-07-18 15:05:15 -0500

    end demo

--- web/backend/app.py
+++ web/backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('template.html')
+	return render_template('template.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
