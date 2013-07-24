33bd7169a8fe3cbed181d1bd19e39ca3b6f408d1
Ubuntu <ubuntu@ip-10-254-39-238.us-west-2.compute.internal>
2013-07-24 15:13:16 +0000

    scraper changes for server migration

--- scrapers/v2/update_script.sh
+++ scrapers/v2/update_script.sh
@@ -2,5 +2,5 @@
 
 source /etc/bashrc
 
-python /home/ec2-user/bikeshare/scrapers/v2/database_scraper.py http://divvybikes.com/stations/json chicago
-python /home/ec2-user/bikeshare/scrapers/v2/database_scraper.py http://citibikenyc.com/stations/json newyork
+python /home/ubuntu/bikeshare/scrapers/v2/database_scraper.py http://divvybikes.com/stations/json chicago
+python /home/ubuntu/bikeshare/scrapers/v2/database_scraper.py http://citibikenyc.com/stations/json newyork
