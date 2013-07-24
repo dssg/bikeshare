07e9b879e459fa1a10dfb315fe793214308c274f
Ubuntu <ubuntu@ip-10-254-39-238.us-west-2.compute.internal>
2013-07-24 16:09:53 +0000

    changes to server changes

--- scrapers/v2/update_script.sh
+++ scrapers/v2/update_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source /etc/bashrc
+source /etc/bash.bashrc
+source /home/ubuntu/.bashrc
+
 
 python /home/ubuntu/bikeshare/scrapers/v2/database_scraper.py http://divvybikes.com/stations/json chicago
 python /home/ubuntu/bikeshare/scrapers/v2/database_scraper.py http://citibikenyc.com/stations/json newyork
