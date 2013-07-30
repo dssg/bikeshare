2310b35f6e33f3acab3140450208cb83a0170377
root <root@ip-10-252-65-133.us-west-2.compute.internal>
2013-07-30 19:33:27 +0000

    scrapers fixes

--- scrapers/v1/update_script.sh
+++ scrapers/v1/update_script.sh
@@ -2,6 +2,6 @@
 
 source /etc/bashrc
 
-python /home/ubuntu/bikeshare/scrapers/v1/database_update_parser.py http://www.thehubway.com/data/stations/bikeStations.xml boston
-python /home/ubuntu/bikeshare/scrapers/v1/database_update_parser.py https://secure.niceridemn.org/data2/bikeStations.xml minneapolis
-python /home/ubuntu/bikeshare/scrapers/v1/database_update_parser.py http://www.capitalbikeshare.com/data/stations/bikeStations.xml washingtondc
+python /home/ec2-user/bikeshare/scrapers/v1/database_update_parser.py http://www.thehubway.com/data/stations/bikeStations.xml boston
+python /home/ec2-user/bikeshare/scrapers/v1/database_update_parser.py https://secure.niceridemn.org/data2/bikeStations.xml minneapolis
+python /home/ec2-user/bikeshare/scrapers/v1/database_update_parser.py http://www.capitalbikeshare.com/data/stations/bikeStations.xml washingtondc
