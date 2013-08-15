f045c1d828b9d757798673677f385d5f14dd3329
Juan-Pablo Velez <jpvelez@gmail.com>
2013-08-14 21:31:36 -0500

    fixed name error in poisson model

--- model/poisson_model.py
+++ model/poisson_model.py
@@ -39,8 +39,8 @@ def find_hourly_arr_dep_deltas(station_updates):
     # Set NaN delta counts to 0
     # By default the resampling step puts NaN (null values) into the data when there were no observations
     # to count up during those thirty minutes. 
-    arrival_departure_deltas = pd.DataFrame(arrivals, columns=["arrivals"])
-    arrival_departure_deltas['departures'] = departures
+    arrival_departure_deltas = pd.DataFrame(pos_interval_counts_null, columns=["arrivals"])
+    arrival_departure_deltas['departures'] = neg_interval_counts_null
 
     arrival_departure_deltas = arrival_departure_deltas.fillna(0)
 
