1cd6d3cc0a9d604e81552291534a846f39b1997f
Juan-Pablo Velez <jpvelez@gmail.com>
2013-08-14 21:52:31 -0500

    rewrote model validation script to take in model type from stdin

--- model/model_validation.py
+++ model/model_validation.py
@@ -2,10 +2,12 @@
 # <nbformat>3.0</nbformat>
 
 # <codecell>
-
+import sys
 import pandas as pd
 from pandas.core.datetools import *
 import numpy as np
+import psycopg2
+import os
 from binomial_fit_function import binomial_fit
 from poisson_fit_function import poisson_fit
 
@@ -48,18 +50,38 @@ def model_validation(modelfit, n, data, stationid, startdate = None):
 
 # <codecell>
 if __name__ == '__main__':
-    print >> sys.stderr, "Getting data for station."
-    from fetch_station import fetch_station
-    stationid = 17
-
-    dc_station_17_poisson = fetch_station("Washington, D.C.", stationid)
 
     start_date = "02/16/2012"
-    print >> sys.stderr, "Running model validation script for poisson."
-    model_validation(poisson_fit, 25, dc_station_17_poisson, stationid, start_date)
+    station_id = 17
+
+    model = sys.argv[1]
+    model = model.lower
+
+    if model == "poisson":
+
+        conn = psycopg2.connect("dbname="+os.environ.get('dbname')+" user="+os.environ.get('dbuser') + " host="+os.environ.get('dburl'))
+        cur = conn.cursor()
+
+        print >> sys.stderr, "Getting data for station."
+        cur.execute("SELECT * FROM bike_ind_washingtondc WHERE tfl_id = " + str(station_id) + ";")
+        station_data = cur.fetchall()
+
+        # Add data to dataframe, add timezone
+        dc_station_17_poisson = pd.DataFrame.from_records(station_data, columns = ["station_id", "bikes_available", "spaces_available", "timestamp"], index = "timestamp")
+        dc_station_17_poisson.index = dc_station_17_poisson.index.tz_localize('UTC').tz_convert('US/Eastern')
+
+        print >> sys.stderr, "Running model validation script for poisson."
+        model_validation(poisson_fit, 25, dc_station_17_poisson, station_id, start_date)
 
 # <codecell>
-    dc_station_17_binomial = fetch_station("Washington, D.C.", stationid, 15, 'max')
-    print >> sys.stderr, "Running model validation script for binomial."
-    model_validation(binomial_fit, 25, dc_station_17_binomial, stationid, start_date)
+    elif model == "binomial":
+
+        from fetch_station import fetch_station
+        dc_station_17_binomial = fetch_station("Washington, D.C.", station_id, 15, 'max')
+
+        print >> sys.stderr, "Running model validation script for binomial."
+        model_validation(binomial_fit, 25, dc_station_17_binomial, station_id, start_date)
+
+    else:
+        print >> sys.stderr, "Can't validate that model!"
 
